686ef8ec0c88015e9dcafa8bb9886790c2d47e8a
Lambros Petrou <lambrospetrou@gmail.com>
2015-09-14 23:11:22 +0100

    added some methods - not finl

--- storage/file_storager.go
+++ storage/file_storager.go
@@ -5,26 +5,104 @@ import (
 	"github.com/lambrospetrou/gomicroblog/post"
 	"io/ioutil"
 	"os"
+	"time"
 )
 
+// postDateFilename returns the Date part of the post filename
+func postDateFilename(dt time.Time) string {
+	return dt.Format("20060102")
+}
+
+// postFileName returns the filename to be used for the blog post
+// when stored on disk.
+func postFilename(data_dir string, bp *post.BPost) string {
+	return data_dir + "/" + postDateFilename(bp.DateCreated) + "-" + id + ".dat"
+}
+
+///////////////////////////////////////////////////
+//----------------
+///////////////////////////////////////////////////
+
+type fssClientBundleAdd struct {
+	ChResult chan error
+	PostNew  *post.BPost
+}
+
+type fssClientBundleRem struct {
+	ChResult chan error
+	PostId   string
+}
+
+type FileSavingService struct {
+	ChPostSave   chan fssClientBundleAdd
+	ChPostRemove chan fssClientBundleRem
+	ChTerminate  chan bool
+	DataDir      string
+}
+
+func newFileSavingService(dt_dir string) *FileSavingService {
+	return &FileSavingService{
+		make(chan fssClientBundleAdd),
+		make(chan fssClientBundleRem),
+		make(chan bool),
+		dt_dir,
+	}
+}
+
+func (fss *FileSavingService) run() {
+	go func(ChTerminate <-chan bool,
+		ChNewPost <-chan fssClientBundleAdd,
+		ChRemPost <-chan fssClientBundleRem) {
+		for {
+			select {
+			case <-ChTerminate:
+				return
+			case np := <-ChNewPost:
+				// save the file and return any error at the response channel
+				jsonBytes, err := json.Marshal(np.PostNew)
+				if err != nil {
+					np.ChResult <- errors.New("Could not convert post to JSON format!")
+				}
+				ioutil.WriteFile(postFileName(fss.DataDir, np.PostNew), jsonBytes, 0777)
+				np.ChResult <- nil
+			case dp := <-ChRemPost:
+				src_f := postFileName(fss.DataDir, dp.PostRem)
+				if os.Remove(src_f) != nil {
+					dp.ChResult <- errors.New("Could not delete post: " + id)
+				}
+				dp.ChResult <- nil
+			}
+		}
+	}(fss.ChTerminate, fss.ChPostSave, fss.ChPostRemove)
+}
+
+// FileStorager is the main object that will be handling the post saving on disk
+// using a file-based approach instead of using a database to allow later easy addition
+// of posts without requiring updates in a database.
 type FileStorager struct {
 	data_dir string
+	fss      *FileSavingService
 }
 
 // New returns a new FileStorager that will use the given dir as a root directory
 // to store, load and delete the blog posts
 func New(dir string) FileStorager {
-	return FileStorager{dir}
+	fs := FileStorager{dir}
+	fs.fss = NewFileSavingService()
 }
 
-func (fs *FileStorager) Store(p *post.BPost) (string, error) {
-	id := p.IdStr()
-	jsonBytes, err := json.Marshal(p)
-	if err != nil {
-		return nil, errors.New("Could not convert post to JSON format!")
+func (fs *FileStorager) Store(p *post.BPost) error {
+	// we have to allocate a channel and send it to the FileSavingService
+	chResult := make(chan error)
+	fs.fss <- fssClientBundleAdd{chResult, p}
+	if err := <-chResult; err != nil {
+		return errors.New("Could not store new post!")
 	}
-	ioutil.WriteFile(postFileName(fs, id), jsonBytes, 0777)
-	return id, nil
+	return nil
+}
+
+func (fs *FileStorager) Delete(id string) error {
+
 }
 
 func (fs *FileStorager) Load(id string) (*post.BPost, error) {
@@ -33,14 +111,6 @@ func (fs *FileStorager) Load(id string) (*post.BPost, error) {
 		return nil, errors.New("Could not find post: " + id)
 	}
 	return post.FromJson(bytes), nil
-}
-
-func (fs *FileStorager) Delete(id string) error {
-	src_f := postFileName(fs, id)
-	if os.Remove(src_f) != nil {
-		return errors.New("Could not delete post: " + id)
-	}
-	return nil
 }
 
 func (fs *FileStorager) LoadAll() ([]post.BPost, error) {
@@ -52,8 +122,4 @@ func (fs *FileStorager) LoadAll() ([]post.BPost, error) {
 		posts = append(posts, post.FromJson(ioutil.ReadFile(fs.data_dir+"/"+f.Name())))
 	}
 	return posts
-}
-
-func postFileName(fs *FileStorager, id string) string {
-	return fs.data_dir + "/" + id + ".dat"
 }
