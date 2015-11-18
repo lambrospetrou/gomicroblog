beba0e3777de9ec5fc3886f412841bf5d742187d
Lambros Petrou <lambrospetrou@gmail.com>
2015-11-18 00:35:49 +0000

    refactorings

--- microblog.go
+++ microblog.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/lambrospetrou/gomicroblog/gen"
 	"log"
 	"path/filepath"
 	"runtime"
+
+	"github.com/lambrospetrou/gomicroblog/gen"
 )
 
 func main() {
@@ -15,18 +16,17 @@ func main() {
 	// use all the available cores
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	var dir_site = flag.String("site", "", "specify a directory that contains the site to generate")
+	var dirSite = flag.String("site", "", "specify a directory that contains the site to generate")
 	flag.Parse()
 
-	log.Println("site:", *dir_site)
-	if len(*dir_site) > 0 {
-		err := gen.GenerateSite(*dir_site, filepath.Join(*dir_site, "config.json"))
+	log.Println("site:", *dirSite)
+	if len(*dirSite) > 0 {
+		err := gen.GenerateSite(*dirSite, filepath.Join(*dirSite, "config.json"))
 		if err != nil {
 			panic(err)
 		}
 		return
-	} else {
-		log.Fatalln("Site source directory not given")
-		return
 	}
+	log.Fatalln("Site source directory not given")
+	return
 }
